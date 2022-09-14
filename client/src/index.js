5dddba136d504f1c63a6259d62182dea1eb20539
nguyen-william93 <=>
2022-09-13 21:36:50 -0500

    saving work for system update

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,26 @@ import {ApolloClient} from '@apollo/client'
 import { InMemoryCache } from '@apollo/client'
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/client'
+import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:5000'
 })
 
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+})
+
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
