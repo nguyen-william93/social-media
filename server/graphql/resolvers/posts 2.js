2ef2107d55c60d81bda0570565a5396393ed6013
nguyen-william93 <=>
2022-10-03 10:08:02 -0500

    update

---
+++ server/graphql/resolvers/posts 2.js
@@ -0,0 +1,101 @@
+const Post =require ("../../models/Post")
+const checkAuth = require('../../Utils/checkAuth')
+const {AuthenticationError, UserInputError} = require('apollo-server');
+
+module.exports = {
+    Query: {
+        getPosts: async() => {
+            try{
+                const posts = await Post.find().sort({ createdAt: -1});
+                return posts;
+            }catch(err){
+                throw new Error(err)
+            }
+        },
+        getPost: async(parent, {postId}) => {
+            try{
+                const post = await Post.findById(postId);
+                if(post){
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                }
+            } catch (err){
+                throw new Error(err)
+            }
+        }
+    },
+
+    Mutation:{
+        createPost: async(parent, {body}, context) => {
+            const user = checkAuth(context);
+
+            if(body.trim() === ''){
+                throw new Error('Post body must not be empty')
+            }
+            
+            const newPost = new Post(
+                {
+                    body,
+                    user: user.indexOf,
+                    username: user.username,
+                    createdAt: new Date().toISOString()
+                }
+            )
+
+            const post = await newPost.save();
+
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            })
+
+            return post
+        },
+
+        deletePost: async(parent, {postId}, context)=>{
+            const user = checkAuth(context);
+            try{
+                const post = await Post.findById(postId);
+                if(user.username === post.username){
+                    await post.delete();
+                    return "Post deleted successfully"
+                } else {
+                    throw new AuthenticationError("Action not allowed")
+                }
+            } catch (err) {
+                throw new Error(err)
+            }
+        },
+        likePost: async(parent, {postId}, context) => {
+            const {username} = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            //post is found
+            if(post){
+                if(post.likes.find(like => like.username === username)){
+                    //post already liked => unlike it
+                    post.likes = post.likes.filter(like => like.username !== username)
+                    await post.save()
+                } else {
+                    //not liked => so like post
+                    post.likes.push(
+                        {
+                            username,
+                            createdAt: new Date().toISOString()
+                        }
+                    )
+                }
+                await post.save();
+                return post;
+            //post is not found
+            } else {
+                throw new UserInputError('Post not found')
+            }
+        }
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (parent, _,{pubsub}) => pubsub.asyncIterator('NEW_POST')
+        }
+    }
+}
---
+++ server/graphql/resolvers/users 2.js
@@ -0,0 +1,84 @@
+const User = require('../../models/User')
+const bcrypt = require ('bcryptjs')
+const jwt = require ('jsonwebtoken')
+const { SECRET_KEY} = require('../../config')
+const {UserInputError} = require('apollo-server')
+const {validateRegisterInput, validateLoginInput} = require('../../Utils/validation')
+
+const generaToken = (user) => {
+    return jwt.sign({
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }, 
+    SECRET_KEY, 
+    {expiresIn: "5h"});
+}
+
+
+module.exports = {
+    Mutation: {
+        register: async (parent, {registerInput: {username, email, password, confirmedPassword}}, context, info) => {
+            //validate user data
+            const {valid, errors} = validateRegisterInput(username, email, password, confirmedPassword);
+            if (!valid){
+                throw new UserInputError('Error',{errors})
+            }
+            //make sure user isn't already exist
+            const user = await User.findOne({ username })
+            if(user){
+                throw new UserInputError("Username is taken", {
+                    errors: {
+                        username: "This username is taken"
+                    }
+                })
+            }
+
+            //hash password
+            password = await bcrypt.hash(password, 12)
+
+            //create new user
+            const newUser = new User (
+                {
+                    email, 
+                    username, 
+                    password, 
+                    createdAt: new Date().toISOString()
+                }
+            )
+            const res = await newUser.save();
+            
+            //sign token
+            const token = generaToken(res)
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        },
+
+        login: async(parent, {username, password}) => {
+            const {valid, errors} = validateLoginInput(username, password);
+            const user = await User.findOne({username});
+            if(!user){
+                errors.general = 'User not found';
+                throw new UserInputError("User not found", {errors})
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if(!match){
+                errors.general = "Wrong credential"
+                throw new UserInputError("Wrong Credential", {errors})
+            }
+
+            const token = generaToken(user);
+
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            }
+        },
+    }
+}
